f67b9e891deceab7379c5ef75e5d4b5a43f93953
grigoryan.m <waldm4rk@gmail.com>
2023-09-06 21:54:19 +0400

    fixed some bugs, added hotkey

--- main.js
+++ main.js
@@ -14,38 +14,39 @@ document.addEventListener("DOMContentLoaded", ()=>{
     
     target.innerText = getRandomNum(360);
 
-    degreeInput.addEventListener('input', () => {
+    function getArrowPosition(){
         const degree = degreeInput.value;
-        const radians = (degree - 90) * (Math.PI / 180); // Преобразование градусов в радианы
-        const radius = circle.offsetWidth / 2; // Радиус круга
-    
-        // Рассчитываем координаты x и y на окружности
+        const radians = (degree - 90) * (Math.PI / 180);
+        const radius = circle.offsetWidth / 2; 
         const x = radius * Math.cos(radians);
         const y = radius * Math.sin(radians);
     
-        // Устанавливаем положение стрелки на окружности
         arrow.style.transform = `translate(-50%, -50%) translate(${x}px, ${y}px) rotate(${degree}deg)`;
-    });
-
-    submitButton.addEventListener("click", ()=>{
-
-    // Получаем угол, выбранный пользователем из input
-    let userAngle = parseInt(angleInput.value);
-
-    if(userAngle === 360){
-        userAngle = 0;
     }
+    getArrowPosition();
+    degreeInput.addEventListener('input', getArrowPosition);
 
-    let correctAngle = parseInt(target.innerText);
-    // Вычисляем разницу между углами и получаем абсолютное значение
-    const angleDifference = Math.abs(correctAngle - userAngle);
-  
-    // Вычисляем процент близости к правильному ответу
-    const accuracyPercentage = 100 - (angleDifference / 360) * 100;
-  
-    // Отображаем процент на странице
-    alert(`Вы были близки к правильному ответу на ${accuracyPercentage.toFixed(0)}%`);
+    function submit(){
+        let userAngle = parseInt(angleInput.value);
 
-    location.reload();
+        if(userAngle === 360){
+            userAngle = 0;
+        }
+
+        let correctAngle = parseInt(target.innerText);
+        const angleDifference = Math.abs(correctAngle - userAngle);
+    
+        const accuracyPercentage = 100 - (angleDifference / 360) * 100;
+    
+        alert(`Your accuracy is ${accuracyPercentage.toFixed(1)}%\nYour angle was ${userAngle}°`);
+
+        location.reload();
+    }
+    submitButton.addEventListener("click", submit);
+
+    document.addEventListener("keydown", (event)=>{
+        if(event.code === "Space"){
+            submit();
+        }
     });
 });
